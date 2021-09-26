e0c3a0a1e9a436a4259abaea88f24791079ea6cd
RaviDurairaj <ravidurairaj89@gmail.com>
2021-09-26 11:09:24 +0530

    Initial commit with Java ForEach function.
    Change-Id: I2343eaf2d5c9e8b9272809d6d77093e03bdcc909

---
+++ src/com/ravi/poc/ForEachJava8.java
@@ -0,0 +1,38 @@
+package com.ravi.poc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * @author ravidurairaj
+ * This class is for understanding the ForEach function
+ */
+public class ForEachJava8 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		forEachExample();
+	}
+	
+	/**
+	 * method to explore the ForEach function of collection
+	 */
+	private static void forEachExample() {
+		List<Integer> myList = new ArrayList<Integer>();
+		for(int i=0;i<=10;i++) myList.add(i);
+		
+		myList.forEach(new Consumer<Integer>() {
+
+			@Override
+			public void accept(Integer t) {
+				System.out.println("inside ananymous "+t);
+			}
+			
+		});
+			
+	}
+
+}
